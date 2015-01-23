f6a9967e61529cc5f2d0460e868b80a6051a505e
evelynwang <younan.wang@mail.utoronto.ca>
2015-01-23 17:36:30 -0500

    modify the matrix inverse code

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv = NULL
+  set = function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse
+  getinv = function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, A) {
         ## Return a matrix that is the inverse of 'x'
+  inv = x$getinv()
+  if(!is.null(inv) && x$get() == A){
+    message("getting cached data")
+    return(inv)
+  }
+  else if(!is.null(inv) && x$get() != A){
+    message("the matrix has been changed")
+    message("inverse of the matrix needs to be recalculated")
+    inv = solve(A)
+    x$set(A)
+    x$setinv(inv)
+    inv
+  }
+  data = x$get()
+  inv = solve(data)
+  x$setinv(inv)
+  inv
 }
